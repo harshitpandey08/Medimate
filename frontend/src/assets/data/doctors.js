7fac262861b207b93eb5e980383ff6681470b9a0
Harshit Pandey <harshitpandey@jklu.edu.in>
2024-11-12 01:34:21 +0530

    Completed Frontend

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,33 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Mithlesh Singh",
+    specialization: "Surgeon",
+    avgRating: 4.8,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "AIIMS Hospital, Delhi.",
+  },
+  {
+    id: "02",
+    name: "Dr. Samiul Haque",
+    specialization: "Neurologist",
+    avgRating: 4.8,
+    photo: doctorImg02,
+    totalPatients: 1300,
+    hospital: "Mount Adora Hospital, Sylhet.",
+  },
+  {
+    id: "03",
+    name: "Dr. Adarsh Singh",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    photo: doctorImg03,
+    totalPatients: 1000,
+    hospital: "SMS Hospital, Jaipur.",
+  },
+];
